1991e260efb17a3f17c4e290ca0f22a362d7b5ba
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-02-11 20:03:38 -0600

    make string calculator dependent on adder

--- general-calculator/string-calculator/src/main/java/StringCalculator.java
+++ general-calculator/string-calculator/src/main/java/StringCalculator.java
@@ -1,8 +1,10 @@
+import forInteger.Adder;
+
 public class StringCalculator {
     public static String concat(String a, String b) { return a + b; }
     public static String addIntgerStrings(String n1, String n2) {
         Integer x = Integer.valueOf(n1), y = Integer.valueOf(n2);
-        x = x+y;
+        x = Adder.add(x,y);
         return x.toString();
     }
 }
